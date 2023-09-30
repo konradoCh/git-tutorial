619f0e39b4f46bb1a097b1fa7015dd31fd550983
Konrad-Fake <konradfake@zajavka.pl>
2023-09-30 07:02:26 +0200

    commit new branch2 -cherry pick

--- Person.java
+++ Person.java
@@ -2,10 +2,9 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
-        System.out.println("test2");
-        System.out.println("test2");
-        System.out.println("test2");
 
+        System.out.println("new branch");
+        System.out.println("new branch 2");
 
     }
 }
