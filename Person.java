b1a149e5bfe3728aaa88a0749decd786203820d6
konrad <konradFake@zajavka.pl>
2023-10-01 15:51:09 +0200

    Pulle request test

--- Person.java
+++ Person.java
@@ -1,10 +1,5 @@
 public class Person {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test2");
-
-        System.out.println("new branch");
-        System.out.println("new branch 2");
-
+        System.out.println("Pull request test");
     }
 }
